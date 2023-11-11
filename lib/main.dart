fde301970ee45214fb755ed45efa40ca0a414b28
takyano2002 <deokishisu2002@gmail.com>
2023-11-11 17:37:40 +0900

    helloが送れた

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,27 @@
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+
+main() async {
+  Socket socket = await Socket.connect('192.168.0.136', 4321);
+  print('connected');
+
+  // listen to the received data event stream
+  socket.listen((List<int> event) {
+    print(utf8.decode(event));
+  });
+
+  // send hello
+  socket.add(utf8.encode('hello'));
+
+  // wait 5 seconds
+  await Future.delayed(Duration(seconds: 5));
+
+  // .. and close the socket
+  socket.close();
+}
+
+/*
 import 'package:flutter/material.dart';
 
 void main() {
@@ -123,3 +147,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
